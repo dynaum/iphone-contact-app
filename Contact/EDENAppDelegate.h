f26388b5def5106a17b82746c486ecd34b634a39
Elber Ribeiro <elber@baby.com.br>
2014-01-13 15:00:56 -0200

    Initial Commit

---
+++ Contact/EDENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EDENAppDelegate.h
+//  Contact
+//
+//  Created by Elber Ribeiro on 1/13/14.
+//  Copyright (c) 2014 Dinda.com.br. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Contact/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contact
+//
+//  Created by Elber Ribeiro on 1/13/14.
+//  Copyright (c) 2014 Dinda.com.br. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EDENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EDENAppDelegate class]));
+    }
+}
