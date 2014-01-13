614944510bf165e1817fae592fa6b1e909adcdab
Elber Ribeiro <elber@baby.com.br>
2014-01-13 15:17:34 -0200

    Adding form view controller

---
+++ Contact/EDENFormContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDENFormContactViewController.h
+//  Contact
+//
+//  Created by Elber Ribeiro on 1/13/14.
+//  Copyright (c) 2014 Dinda.com.br. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDENFormContactViewController : UIViewController
+
+@end
---
+++ Contact/EDENFormContactViewController.m
@@ -0,0 +1,38 @@
+//
+//  EDENFormContactViewController.m
+//  Contact
+//
+//  Created by Elber Ribeiro on 1/13/14.
+//  Copyright (c) 2014 Dinda.com.br. All rights reserved.
+//
+
+#import "EDENFormContactViewController.h"
+
+@interface EDENFormContactViewController ()
+
+@end
+
+@implementation EDENFormContactViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
